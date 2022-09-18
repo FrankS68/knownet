239643a8771282c306e8edcbe4124a4b37dc69eb
FrankS <frank.segieth@gmail.com>
2022-09-18 15:20:45 +0200

    adding primefaces and mongo

--- src/main/java/de/witchcafe/knownet/KnownetApplication.java
+++ src/main/java/de/witchcafe/knownet/KnownetApplication.java
@@ -1,8 +1,21 @@
 package de.witchcafe.knownet;
 
+import java.util.EnumSet;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.DispatcherType;
+
+import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.sun.faces.config.ConfigureListener;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "de.witchcafe")
@@ -12,5 +25,32 @@ public class KnownetApplication {
 		SpringApplication.run(KnownetApplication.class, args);
 	}
 
-	// resources/META-INF/resources
-}
+	
+    @Bean
+    public ServletRegistrationBean facesServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean<>(new FacesServlet(), "*.xhtml");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+        return servletContext -> {
+            servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+            servletContext.setInitParameter("primefaces.THEME", "nova-light");
+        };
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+        return new ServletListenerRegistrationBean<>(new ConfigureListener());
+    }
+    
+    @Bean
+	public FilterRegistrationBean rewriteFilter() {
+		FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
+		rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
+				DispatcherType.ASYNC, DispatcherType.ERROR));
+		rwFilter.addUrlPatterns("/*");
+		return rwFilter;
+	}}
