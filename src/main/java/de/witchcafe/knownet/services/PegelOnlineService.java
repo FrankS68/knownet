0c01cffd1f0f3f250c801f00cd0a73880461fb5d
FrankS <frank.segieth@gmail.com>
2022-09-22 21:50:18 +0200

    make PegelOnlineService 99% generic

--- src/main/java/de/witchcafe/knownet/services/PegelOnlineService.java
+++ src/main/java/de/witchcafe/knownet/services/PegelOnlineService.java
@@ -1,47 +1,14 @@
 package de.witchcafe.knownet.services;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
-import de.witchcafe.base.StatusController.Status;
-import de.witchcafe.knownet.Service;
-
-public class PegelOnlineService extends Service {
-	List<String> list;
+public class PegelOnlineService extends RestProxy {
 	public PegelOnlineService() {
 		super();
-		RestTemplate restTemplate = new RestTemplate();
-		URI url = null;
-		try {
-			url = new URI("https://pegelonline.wsv.de/webservices/rest-api/v2/waters.json");
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		statusController.log(getClass().getCanonicalName(), Status.info, "load service");
-		ResponseEntity<List> resp = restTemplate.getForEntity(url, List.class);
-		list = resp.getBody();
-		statusController.log(getClass().getCanonicalName(), Status.info, String.format("loaded %s entries",list.size()));
+		setNameKey("longname");
+		setIdKey("shortname");
+		setName("Waters");
+		setId("de.wsv.pegelonline.Waters");
+		setEndPoint("https://pegelonline.wsv.de/webservices/rest-api/v2/waters.json");
+		loadData();
 	}
 	
-	@Override
-	public Collection getEntries() {
-		return list;
-	}
-
-	@Override
-	public String getNameKey() {
-		return "longname";
-	}
-
-	@Override
-	public String getIdKey() {
-		return "shortname";
-	}
-		
 }
---
+++ src/main/java/de/witchcafe/knownet/services/RestProxy.java
@@ -0,0 +1,88 @@
+package de.witchcafe.knownet.services;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import de.witchcafe.base.StatusController.Status;
+import de.witchcafe.knownet.Service;
+
+public class RestProxy extends Service {
+
+	private String idKey;
+	private String nameKey;
+	private String id;
+	private String name;
+	protected List<Map<String, Object>> list;
+	protected RestTemplate restTemplate = new RestTemplate();
+	private String endPoint;
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	public void setIdKey(String idKey) {
+		this.idKey = idKey;
+	}
+
+	@Override
+	public String getIdKey() {
+		return idKey;
+	}
+
+	public void setNameKey(String nameKey) {
+		this.nameKey = nameKey;
+	}
+
+	@Override
+	public String getNameKey() {
+		return nameKey;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public Collection getEntries() {
+		return list;
+	}
+
+	protected String getEndPoint() {
+		return endPoint;
+	}
+
+	protected void setEndPoint(String endPoint) {
+		this.endPoint = endPoint;
+	}
+
+	protected void loadData() {
+		URI url = null;
+		try {
+			url = new URI(getEndPoint());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		statusController.log(getClass().getCanonicalName(), Status.info, "load service");
+		ResponseEntity<java.util.List> resp = restTemplate.getForEntity(url, java.util.List.class);
+		list = resp.getBody();
+		statusController.log(getClass().getCanonicalName(), Status.info, String.format("loaded %s entries",list.size()));
+	}
+}
